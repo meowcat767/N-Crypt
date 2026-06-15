469c67b5bfcdc3441096e96e92dd7cad8e3c5d3d
Ben <meowcat@meowcat.site>
2026-06-15 09:52:58 +0100

    ui done!

--- src/main/java/site/meowcat/Main.java
+++ src/main/java/site/meowcat/Main.java
@@ -1,7 +1,16 @@
 package site.meowcat;
+import site.meowcat.ui.Interface;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("N-Crypt");
+            frame.setContentPane(new Interface().getContentPane());
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
--- src/main/java/site/meowcat/ui/Interface.java
+++ src/main/java/site/meowcat/ui/Interface.java
@@ -9,8 +9,45 @@ public class Interface {
     private JTabbedPane tabbedPane1;
     private JTextArea privateKeyArea;
     private JTextArea publicKeyArea;
+    private JPanel textEncryptPanel;
+    private JTextArea textIOArea;
+    private JButton encryptButton;
+    private JButton decryptButton;
+    private JPanel imageEncryptPanel;
+    private JButton selectImageButton;
+    private JLabel imagePathLabel;
+    private JLabel imagePreviewLabel;
+    private JButton encryptImageButton;
+    private JButton decryptImageButton;
+    private JButton openButton;
+    private JButton saveButton;
+    private JButton exitButton;
+    private JPanel fileEncryptPanel;
+    private JButton selectFileButton;
+    private JLabel filePathLabel;
+    private JButton encryptFileButton;
+    private JButton decryptFileButton;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    public Interface() {
+        selectImageButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int result = fileChooser.showOpenDialog(contentFrame);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                imagePathLabel.setText(fileChooser.getSelectedFile().getAbsolutePath());
+                // TODO: Image Preview Logic
+            }
+        });
+        selectFileButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int result = fileChooser.showOpenDialog(contentFrame);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                filePathLabel.setText(fileChooser.getSelectedFile().getAbsolutePath());
+            }
+        });
+        exitButton.addActionListener(e -> System.exit(0));
+    }
+
+    public JPanel getContentPane() {
+        return contentFrame;
     }
 }
